b9cf76480da1b8fcf203c3d7121e062f618dcd0b
Ivan Kolesnikov <i.kolesnikov@ritg.ru>
2022-01-11 00:43:02 +0400

    swagger

--- apps/users/views.py
+++ apps/users/views.py
@@ -1,3 +1,4 @@
+from drf_spectacular.utils import extend_schema
 from rest_framework import permissions, viewsets
 from rest_framework.decorators import action
 from rest_framework.response import Response
@@ -10,6 +11,7 @@ class UsersView(viewsets.GenericViewSet):
     """Класс для работы с пользователями"""
     serializer_class = UsersSerializers
 
+    @extend_schema(tags=['Пользователи'], description='Получение аутенфтицированного пользователя')
     @action(detail=False, methods=['get'], permission_classes=(permissions.IsAuthenticated,))
     def current_user(self, request):
         user = UserProfile.objects.all().filter(user=request.user).first()
